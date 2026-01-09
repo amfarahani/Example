66386032c2dcade12b42f406eb4d23f69d34792a
Ali Farahani <afarahan@nu.edu>
2026-01-09 13:58:27 -0800

    Added new code

--- Greet.java
+++ Greet.java
@@ -1,5 +1,7 @@
 public class Greet{
    public static void main (String [] args) {
-      System.out.println("Hello All!");
+      System.out.println("Hello All! add this line");
+      System.out.println("Happy New Year");
+      System.out.println("one more line");
    }
 }
